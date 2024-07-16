252e05a3391169099f96fa2935f0e76fa8f8e12f
MPmichal <pawlaczek955@o2.pl>
2024-07-16 16:40:55 +0200

    9- import dużego pliku

---
+++ src/main/java/pl/kurs/AsyncConfig.java
@@ -0,0 +1,22 @@
+package pl.kurs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfig {
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("BookImport-");
+        executor.initialize();
+        return executor;
+    }
+}
