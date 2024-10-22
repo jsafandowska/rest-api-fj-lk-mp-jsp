7284e57277169677eefe92e26cbd28278de20ad3
heryszek <heryszek@gmail.com>
2024-10-22 22:05:52 +0100

    Zadanie domowe

--- src/main/java/pl/kurs/config/AsyncConfig.java
+++ src/main/java/pl/kurs/config/AsyncConfig.java
@@ -1,22 +1,35 @@
 package pl.kurs.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
+
 @Configuration
 public class AsyncConfig {
 
-    // todo dodac te wartosci do properties, albo ustawic je w klasie konfiguracyjnej
+    @Value("${async.books.import.corePoolSize}")
+    private int corePoolSize;
+
+    @Value("${async.books.import.maxPoolSize}")
+    private int maxPoolSize;
+
+    @Value("${async.books.import.queueCapacity}")
+    private int queueCapacity;
+
+    @Value("${async.books.import.threadNamePrefix}")
+    private String threadNamePrefix;
+
     @Bean
     public Executor booksImportExecutor() {
         final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(1);
-        executor.setMaxPoolSize(1);
-        executor.setQueueCapacity(100);
-        executor.setThreadNamePrefix("books-import-");
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setThreadNamePrefix(threadNamePrefix);
         executor.initialize();
         return executor;
     }
--- src/main/java/pl/kurs/controller/GarageController.java
+++ src/main/java/pl/kurs/controller/GarageController.java
@@ -1,5 +1,6 @@
 package pl.kurs.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -21,29 +22,34 @@ public class GarageController {
 
     private final GarageService garageService;
 
+    @Operation(summary = "Test transactions handling in garages")
     @GetMapping("/test")
     public void test() {
         garageService.playWithTransactions();
     }
 
+    @Operation(summary = "Find all garages with pagination")
     @GetMapping
     public ResponseEntity<Page<GarageDto>> findAll(@PageableDefault Pageable pageable) {
         log.info("findAll");
         return ResponseEntity.ok(garageService.findAllGarages(pageable).map(GarageDto::toDto));
     }
 
+    @Operation(summary = "Add a new garage")
     @PostMapping
     public ResponseEntity<GarageDto> addGarage(@RequestBody CreateGarageCommand command) {
         log.info("addGarage({})", command);
         return ResponseEntity.status(HttpStatus.CREATED).body(GarageDto.toDto(garageService.addGarage(command)));
     }
 
+    @Operation(summary = "Find a specific garage by its ID")
     @GetMapping("/{id}")
     public ResponseEntity<GarageDto> findGarage(@PathVariable int id) {
         log.info("findGarage({})", id);
         return ResponseEntity.ok(GarageDto.toDto(garageService.findGarage(id)));
     }
 
+    @Operation(summary = "Delete a specific garage by its ID")
     @DeleteMapping("/{id}")
     public ResponseEntity<GarageDto> deleteGarage(@PathVariable int id) {
         log.info("deleteGarage({})", id);
@@ -51,18 +57,21 @@ public class GarageController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @Operation(summary = "Edit a specific garage by its ID")
     @PutMapping("/{id}")
     public ResponseEntity<GarageDto> editGarage(@PathVariable int id, @RequestBody CreateGarageCommand command) {
         log.info("editGarage({}, {})", id, command);
         return ResponseEntity.status(HttpStatus.OK).body(GarageDto.toDto(garageService.editGarage(id, command)));
     }
 
+    @Operation(summary = "Partially edit a specific garage by its ID")
     @PatchMapping("/{id}")
     public ResponseEntity<GarageDto> editGaragePartially(@PathVariable int id, @RequestBody EditGarageCommand command) {
         log.info("editGarage({}, {})", id, command);
         return ResponseEntity.status(HttpStatus.OK).body(GarageDto.toDto(garageService.editGaragePartially(id, command)));
     }
 
+    @Operation(summary = "Add a car to a specific garage")
     @PatchMapping("/{id}/cars/{carId}")
     public ResponseEntity<GarageDto> addCar(@PathVariable int id, @PathVariable int carId) {
         log.info("addCar({}, {})", id, carId);
@@ -70,9 +79,10 @@ public class GarageController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Delete a car from a specific garage")
     @DeleteMapping("/{id}/cars/{carId}")
     public ResponseEntity<GarageDto> deleteCarFromGarage(@PathVariable int id, @PathVariable int carId) {
-       log.info("deleteCarFromGarage({}, {})", id, carId);
+        log.info("deleteCarFromGarage({}, {})", id, carId);
         garageService.deleteCarFromGarage(id, carId);
         return ResponseEntity.ok().build();
     }
