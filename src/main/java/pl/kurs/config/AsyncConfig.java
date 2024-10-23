ba8acaacb8c6b04ef9dbed0ddebc44eaf668c7c0
FJarzebowicz <franek.jarzebowicz@gmail.com>
2024-10-23 17:04:27 +0200

    homework

--- src/main/java/pl/kurs/config/AsyncConfig.java
+++ src/main/java/pl/kurs/config/AsyncConfig.java
@@ -1,5 +1,6 @@
 package pl.kurs.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -9,13 +10,25 @@ import java.util.concurrent.Executor;
 @Configuration
 public class AsyncConfig {
 
+    @Value("${async.books.import.corePoolSize}")
+    private int corePoolSize;
+
+    @Value("${async.books.import.maxPoolSize}")
+    private int maxPoolSize;
+
+    @Value("${async.books.import.queueCapacity}")
+    private int queueCapacity;
+
+    @Value("${async.books.import.threadNamePrefix}")
+    private String threadNamePrefix;
+
     @Bean
     public Executor booksImportExecutor() {
         final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(1);
-        executor.setMaxPoolSize(1);
-        executor.setQueueCapacity(100);
-        executor.setThreadNamePrefix("books-import-");
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setThreadNamePrefix(threadNamePrefix);
         executor.initialize();
         return executor;
     }
---
+++ src/main/java/pl/kurs/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package pl.kurs.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI mySwagger(){
+        Server prodServer = new Server();
+        prodServer.setUrl("http://localhost:8080");
+        prodServer.setDescription("Server URL in production environment");
+
+        Info info = new Info()
+                .title("Library API")
+                .version("1.0");
+
+        return new OpenAPI().info(info).servers(List.of(prodServer));
+    }
+}
